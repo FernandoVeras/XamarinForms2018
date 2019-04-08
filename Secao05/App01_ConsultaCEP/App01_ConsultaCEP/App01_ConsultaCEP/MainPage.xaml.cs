a8bd75b966dcd019b65d48a07c932c0146b300e1
Fernando Henrique Santos Veras <fernandoveras@hotmail.com.br>
2019-04-08 16:55:28 -0300

    aula até 09 - valida-es

---
+++ Secao05/App01_ConsultaCEP/App01_ConsultaCEP/App01_ConsultaCEP/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using App01_ConsultaCEP.Servico;
+using App01_ConsultaCEP.Servico.Modelo;
+
+namespace App01_ConsultaCEP
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            btnPesquisar.Clicked += BuscarCep;
+        }
+
+        private void BuscarCep(object sender, EventArgs e)
+        {
+            var cep = txtCep.Text.Trim();
+
+            var endereco = ViaCEPServico.BuscarEnderecoViaCEP(cep);
+
+            lbResultado.Text = string.Format("Endereço é: {0},{1},{2}", endereco.logradouro, endereco.bairro, endereco.localidade);
+        }
+    }
+}
---
+++ Secao05/App01_ConsultaCEP/App01_ConsultaCEP/App01_ConsultaCEP/Servico/ViaCEPServico.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using App01_ConsultaCEP.Servico.Modelo;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace App01_ConsultaCEP.Servico
+{
+    public class ViaCEPServico
+    {
+        private static readonly string EnderecoURL = "http://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCEP(string cep)
+        {
+            try
+            {
+                var novoEnderecoUrl = string.Format(EnderecoURL, cep);
+
+                var client = new WebClient();
+                var stringRetornada = client.DownloadString(novoEnderecoUrl);
+
+                return JsonConvert.DeserializeObject<Endereco>(stringRetornada);
+            }
+            catch (Exception e)
+            {
+                return new Endereco();
+            }
+        }
+    }
+}
